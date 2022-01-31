27861afcf24534f1b23351317878b1a61cc52106
FcoPRO <94026913+FcoPRO@users.noreply.github.com>
2022-01-31 13:46:32 -0600

    Add files via upload

--- js/Player.js
+++ js/Player.js
@@ -10,10 +10,32 @@ class Player {
     this.height = height;
     this.image = loadImage("./assets/player.png");
 
+    this.life1 = "green";
+    this.life2 = "green";
+    this.life3 = "green";
+
     World.add(world, this.body);
   }
 
+  life() {
+    push();
+   //escribe el código correcto para mostrar las vidas como tres rectángulos verdes en una línea horizontal.
+    textSize(20);
+    fill("white");
+    text("Player", 310, 40);
+
+    fill(this.life1);
+    rect(350, 50, 70, 30);
+    fill(this.life2);
+    rect(280, 50, 70, 30);
+    fill(this.life3);
+    rect(210, 50, 70, 30);
+    pop();
+  }
+
  
+
+
   display() {
     var pos = this.body.position;
     var angle = this.body.angle;
